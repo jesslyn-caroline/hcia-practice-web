b42f0f1961a66ef7b7c41e4717cc6d5ddf6395ea
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-24 23:34:52 +0700

    update: login page

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,10 +1,47 @@
-
+import { Link } from "react-router"
 
 function Login() {
     return(
         <div className={`w-full flex justify-center`}>
-            <div className={`max-w-[1624px] max-h-[1284px] w-screen h-screen bg-primary flex justify-center items-center`}>
+            <div className={`max-w-[1620px] max-h-[1040px] w-screen h-screen bg-background flex flex-col`}>
                 
+                {/* Header */}
+                <div className={`w-full min-h-14 h-14 bg-accent border-b-1 border-greyist px-10 lg:px-20 flex items-center`}>
+                    <h1 className={`text-xl text-primary font-semibold`}>HCIA Practice</h1>
+                </div>
+
+                
+                <div className={`w-full h-full px-10 lg:px-20 grid grid-cols-1 lg:grid-cols-2 lg:space-x-20`}>
+
+                    <div className={`w-full h-full flex items-center justify-center`}>
+                        {/* Login Card */}
+                        <div className={`max-w-[500px] w-full h-fit border rounded-xl border-greyist py-8 px-10 flex flex-col items-center space-y-10`}>
+                            <h1 className={`text-xl text-text font-semibold`}>Log In</h1>
+
+                            <div className={`w-full flex flex-col space-y-8`}>
+                                <div className={`w-full`}>
+                                    <label className={`text-md text-text font-medium`}>Student ID</label>
+                                    <input type="text" className={`w-full h-10 outline-none border-b-2 border-text text-md`} id="studentID" placeholder="Enter your Student ID"></input>
+                                </div>
+                                <div className={`w-full`}>
+                                    <label className={`text-md text-text font-medium`}>Password</label>
+                                    <input type="password" className={`w-full h-10 outline-none border-b-2 border-text text-md`} id="password" placeholder="Enter your password"></input>
+                                </div>
+                                <button className={`w-full h-fit bg-primary py-2 rounded-md`}>
+                                    <span className={`text-white text-lg font-medium`}>Log In</span>
+                                </button>
+                            </div>
+
+                            <div className={`w-full`}>
+                                <h3 className={`text-md`}>Don't have an account? <Link to="/signup" className={``}>Sign up</Link></h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={`hidden lg:flex flex items-center`}>
+                        <img src="/images/data-extraction-pana.png" alt="" className={`w-lg`}/>
+                    </div>
+                </div>
             </div>
         </div>    
     )
