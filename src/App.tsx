daa650798f983e9f77e133f4eb4745ac48c48ec5
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-25 14:59:21 +0700

    update: use layout for login and signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { Routes, Route } from 'react-router'
 
-import Login from './screens/login'
-import Layout from './screens/layout'
-import Signup from './screens/signup'
+import LoginSignupLayout from './screens/login_signup_layout.tsx'
+import LoginCard from './components/login_card.tsx'
+import SignupCard from './components/signup_card.tsx'
+
+import Layout from './screens/layout.tsx'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={<Layout />} >
-        <Route index element={<></>} />
-        
+      <Route path="/" element={<LoginSignupLayout />} >
+        <Route path="/login" element={<LoginCard />} />
+        <Route path="/signup" element={<SignupCard />} />
+      </Route>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<h1>Home</h1>} />
       </Route>
     </Routes>
   )
