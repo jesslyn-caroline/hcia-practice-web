5c35da9ff0303addc814253d987925a7d95d6fcf
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-25 15:00:11 +0700

    use layout for consistency

---
+++ src/screens/login_signup_layout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from 'react-router'
+
+function LoginSignupLayout() {
+    return(
+        <div className={`w-full flex justify-center`}>
+            <div className={`max-w-[1620px] max-h-[1040px] h-screen w-screen bg-background flex flex-col`}>
+                
+                {/* Header */}
+                <div className={`w-full min-h-14 h-14 bg-accent border-b-1 border-greyist px-10 lg:px-20 flex items-center`}>
+                    <h1 className={`text-xl text-primary font-semibold`}>HCIA Practice</h1>
+                </div>
+
+                <div className={`w-full h-full px-10 lg:px-20 grid grid-cols-1 lg:grid-cols-2 lg:space-x-20`}>
+                    <Outlet />
+                    <div className={`hidden lg:block pt-12`}>
+                        <img src="/images/data-extraction-pana.png" alt="" className={`w-md`}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignupLayout
