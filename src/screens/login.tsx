28ddef00f160ff4c3213e3e46b134cbffe56ba70
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-25 12:42:32 +0700

    fix: smaller font-size, space between button and "dont have account" text, image size, max width card

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -15,31 +15,31 @@ function Login() {
 
                     <div className={`w-full h-full flex items-center justify-center`}>
                         {/* Login Card */}
-                        <div className={`max-w-[500px] w-full h-fit border rounded-xl border-greyist py-8 px-6 lg:px-10 flex flex-col items-center space-y-8 lg:space-y-10`}>
-                            <h1 className={`text-xl text-text font-semibold`}>Log In</h1>
+                        <div className={`max-w-[400px] w-full h-fit border rounded-xl border-greyist py-8 px-6 lg:px-10 flex flex-col items-center`}>
+                            <h1 className={`text-lg text-text font-semibold`}>Log In</h1>
 
-                            <div className={`w-full flex flex-col space-y-8`}>
-                                <div className={`w-full text-md text-text `}>
+                            <div className={`w-full mt-10 flex flex-col space-y-8`}>
+                                <div className={`w-full text-sm text-text `}>
                                     <label className={`font-medium`}>Student ID</label>
                                     <input type="text" className={`w-full h-10 outline-none border-b-2 border-text`} id="studentID" placeholder="Enter your Student ID"></input>
                                 </div>
-                                <div className={`w-full text-md text-text `}>
+                                <div className={`w-full text-sm text-text `}>
                                     <label className={`font-medium`}>Password</label>
                                     <input type="password" className={`w-full h-10 outline-none border-b-2 border-text`} id="password" placeholder="Enter your password"></input>
                                 </div>
                                 <button className={`w-full h-fit bg-primary py-2 rounded-md`}>
-                                    <span className={`text-white text-lg font-medium`}>Log In</span>
+                                    <span className={`text-white text-sm font-medium`}>Log In</span>
                                 </button>
                             </div>
 
-                            <div className={`w-full`}>
-                                <h3 className={`text-md`}>Don't have an account? <Link to="/signup" className={``}>Sign up</Link></h3>
+                            <div className={`w-full mt-6`}>
+                                <h3 className={`text-sm`}>Don't have an account? <Link to="/signup" className={`text-blue-600 underline`}>Sign up</Link></h3>
                             </div>
                         </div>
                     </div>
 
                     <div className={`hidden lg:flex items-center`}>
-                        <img src="/images/data-extraction-pana.png" alt="" className={`w-lg`}/>
+                        <img src="/images/data-extraction-pana.png" alt="" className={`w-md`}/>
                     </div>
                 </div>
             </div>
