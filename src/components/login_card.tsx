aacaa7134cd917172f65e75d224b0147576f3e22
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-25 20:53:41 +0700

    update: use Context to handle input and login

--- src/components/login_card.tsx
+++ src/components/login_card.tsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router"
+import { useContext } from "react"
+
+import { UserContext } from "../provider/user_context"
 
 function LoginCard() {
+
+    const { studentID, password, handleStudentIDchange, handlePasswordChange, login } = useContext(UserContext)
+
     return(
         <div className={`w-full h-full flex justify-center pt-14`}>
             {/* Login Card */}
@@ -10,13 +16,13 @@ function LoginCard() {
                 <div className={`w-full mt-10 flex flex-col space-y-8`}>
                     <div className={`w-full text-sm text-text `}>
                         <label className={`font-medium`}>Student ID</label>
-                        <input type="text" className={`w-full h-10 outline-none border-b-2 border-text`} id="studentID" placeholder="Enter your Student ID"></input>
+                        <input type="text" value={studentID} onChange={handleStudentIDchange} className={`w-full h-10 outline-none border-b-2 border-text`} id="studentID" placeholder="Enter your Student ID"></input>
                     </div>
                     <div className={`w-full text-sm text-text `}>
                         <label className={`font-medium`}>Password</label>
-                        <input type="password" className={`w-full h-10 outline-none border-b-2 border-text`} id="password" placeholder="Enter your password"></input>
+                        <input type="password" value={password} onChange={handlePasswordChange} className={`w-full h-10 outline-none border-b-2 border-text`} id="password" placeholder="Enter your password"></input>
                     </div>
-                    <button className={`w-full h-fit bg-primary py-2 rounded-md`}>
+                    <button className={`w-full h-fit bg-primary py-2 rounded-md`} onClick={login}>
                         <span className={`text-white text-sm font-medium`}>Log In</span>
                     </button>
                 </div>
