8f2fe0edfa1653a2279a05cf998cb4f67576c837
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-26 13:00:25 +0700

    remove studentID and password context, fix on variable naming for userif

--- src/components/login_card.tsx
+++ src/components/login_card.tsx
@@ -5,7 +5,7 @@ import { UserContext } from "../provider/user_context"
 
 function LoginCard() {
 
-    const { studentID, password, handleStudentIDchange, handlePasswordChange, login } = useContext(UserContext)
+    const { handleUserIdChange, handlePasswordChange, login } = useContext(UserContext)
 
     return(
         <div className={`w-full h-full flex justify-center pt-14`}>
@@ -16,11 +16,11 @@ function LoginCard() {
                 <div className={`w-full mt-10 flex flex-col space-y-8`}>
                     <div className={`w-full text-sm text-text `}>
                         <label className={`font-medium`}>Student ID</label>
-                        <input type="text" value={studentID} onChange={handleStudentIDchange} className={`w-full h-10 outline-none border-b-2 border-text`} id="studentID" placeholder="Enter your Student ID"></input>
+                        <input type="text" onChange={handleUserIdChange} className={`w-full h-10 outline-none border-b-2 border-text`} id="userId" placeholder="Admin ID / Student iD"></input>
                     </div>
                     <div className={`w-full text-sm text-text `}>
                         <label className={`font-medium`}>Password</label>
-                        <input type="password" value={password} onChange={handlePasswordChange} className={`w-full h-10 outline-none border-b-2 border-text`} id="password" placeholder="Enter your password"></input>
+                        <input type="password"  onChange={handlePasswordChange} className={`w-full h-10 outline-none border-b-2 border-text`} id="password" placeholder="Enter your password"></input>
                     </div>
                     <button className={`w-full h-fit bg-primary py-2 rounded-md`} onClick={login}>
                         <span className={`text-white text-sm font-medium`}>Log In</span>
