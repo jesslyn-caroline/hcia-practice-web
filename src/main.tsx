4428b52021ce1554e87d91c20e770b034ac97f8c
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-27 23:33:25 +0700

    move UserProvider to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 
 import App from './App.tsx'
+import UserProvider from './provider/user_context.tsx'
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>,
 )
