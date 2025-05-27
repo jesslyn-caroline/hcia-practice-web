641e468b69397878e50ec2f09f43ed47bcca3165
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-27 21:33:18 +0700

    update: add error message if user doesnt input field

--- src/components/login_card.tsx
+++ src/components/login_card.tsx
@@ -5,7 +5,7 @@ import { UserContext } from "../provider/user_context"
 
 function LoginCard() {
 
-    const { handleUserIdChange, handlePasswordChange, login } = useContext(UserContext)
+    const { userIdErrMessage, passwordErrMessage, handleUserIdChange, handlePasswordChange, login } = useContext(UserContext)
 
     return(
         // <form action="https://huawei-practice-web-backend.vercel.app/api/user" method="post">
@@ -17,10 +17,12 @@ function LoginCard() {
                         <div className={`w-full text-sm text-text `}>
                             <label className={`font-medium`}>User ID</label>
                             <input type="text" onChange={handleUserIdChange} className={`w-full h-10 outline-none border-b-2 border-text`} id="userId" placeholder="Admin ID / Student ID"></input>
+                            <div className={`${userIdErrMessage === "" ? "hidden" : ""} absolute text-xs text-red-500 mt-1`}>{userIdErrMessage}</div>
                         </div>
                         <div className={`w-full text-sm text-text `}>
                             <label className={`font-medium`}>Password</label>
                             <input type="password" onChange={handlePasswordChange} className={`w-full h-10 outline-none border-b-2 border-text`} id="password" placeholder="Enter your password"></input>
+                            <div className={`${passwordErrMessage === "" ? "hidden" : ""} absolute text-xs text-red-500 mt-1`}>{passwordErrMessage}</div>
                         </div>
                         <button className={`w-full h-fit bg-primary py-2 rounded-md`} onClick={login}>
                             <span className={`text-white text-sm font-medium`}>Log In</span>
