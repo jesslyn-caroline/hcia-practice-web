b7c942a60c8e7760b765efd7b09e91d230c6c9c1
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-29 19:48:17 +0700

    new component

---
+++ src/components/toast/toast_error.tsx
@@ -0,0 +1,17 @@
+import { toast, Bounce } from "react-toastify"
+
+function toast_error(message: string) {
+    toast.error(message, {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+    })
+}
+
+export default toast_error
---
+++ src/components/toast/toast_success.tsx
@@ -0,0 +1,17 @@
+import { toast, Bounce } from "react-toastify"
+
+function toast_success (message: string) {
+    toast.success(message, {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+    })
+}
+
+export default toast_success
