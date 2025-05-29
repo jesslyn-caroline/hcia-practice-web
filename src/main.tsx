2246516d16b589640e1194ba5616fe4d83b6c8a1
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-29 19:46:54 +0700

    wrap App with ErrorMessageProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 import UserProvider from './provider/user_context.tsx'
 import CreateQuestionProvider from './provider/create_question_context.tsx'
+import ErrorMessageProvider from './provider/error_message_context.tsx'
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <CreateQuestionProvider>
-          <App />
-        </CreateQuestionProvider>
-      </UserProvider>
+      <ErrorMessageProvider>
+        <UserProvider>   
+          <CreateQuestionProvider>
+            <App />
+          </CreateQuestionProvider>
+        </UserProvider>
+      </ErrorMessageProvider>
     </BrowserRouter>
   </StrictMode>,
 )
