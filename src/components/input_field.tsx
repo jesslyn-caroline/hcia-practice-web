9bc31a750356a6aa45b360bfd8d8bb581941a190
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-30 20:43:58 +0700

    change border color

--- src/components/input_field.tsx
+++ src/components/input_field.tsx
@@ -13,7 +13,7 @@ function InputField({handleInputChange, inputType, errMessage, placeholderValue,
             <label className={`font-medium`}>{labelValue}</label>
             <input type={inputType} 
                 onChange={handleInputChange} 
-                className={`w-full h-9 outline-none border-b-2 border-text`} 
+                className={`w-full h-9 outline-none border-b-2 border-accent-2`} 
                 id={idValue}
                 placeholder={placeholderValue} />
             <div className={`${errMessage === "" ? "hidden" : ""} absolute text-xs text-red-500 mt-1`}>{errMessage}</div>
