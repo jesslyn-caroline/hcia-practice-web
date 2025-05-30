af6fcb8c7c4fb390c17910e7e06174cb19722c1b
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-30 20:43:34 +0700

    new component

---
+++ src/components/input_field.tsx
@@ -0,0 +1,24 @@
+interface Props {
+    handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    inputType: string
+    errMessage: string
+    placeholderValue: string
+    idValue: string
+    labelValue: string
+}
+
+function InputField({handleInputChange, inputType, errMessage, placeholderValue, idValue, labelValue} : Props) {
+    return (
+        <div className={`w-full`}>
+            <label className={`font-medium`}>{labelValue}</label>
+            <input type={inputType} 
+                onChange={handleInputChange} 
+                className={`w-full h-9 outline-none border-b-2 border-text`} 
+                id={idValue}
+                placeholder={placeholderValue} />
+            <div className={`${errMessage === "" ? "hidden" : ""} absolute text-xs text-red-500 mt-1`}>{errMessage}</div>
+        </div>
+    )
+}
+
+export default InputField
---
+++ src/components/toggle_mode.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+function ToggleMode() {
+    const [theme, setTheme] = useState<string>(() => {
+        let currentTheme = JSON.parse(localStorage.getItem("theme") || "light")
+
+        const html = document.documentElement
+        html.setAttribute("data-theme", currentTheme)
+
+        return currentTheme
+    })
+
+    function changeTheme():void {
+        const html = document.documentElement
+        const currentTheme = html.getAttribute("data-theme")
+
+        if (currentTheme === "light") {
+            setTheme("dark")
+
+            html.setAttribute("data-theme", "dark")
+            localStorage.setItem("theme", JSON.stringify("dark"))
+        }
+        else {
+            setTheme("light")
+
+            html.setAttribute("data-theme", "light")
+            localStorage.setItem("theme", JSON.stringify("light"))
+        }
+    }
+
+    return (
+        <button className={`w-10 h-10 2xl:w-12 2xl:h-12 bg-primary rounded-md outline-none cursor-pointer`} onClick={() => changeTheme()} title="Toggle Mode">
+            <i className={`${theme === "light" ? "ri-moon-clear-line" : "ri-sun-line"} text-2xl 2xl:text-3xl text-white`}/>
+        </button>
+    )
+}
+
+export default ToggleMode
