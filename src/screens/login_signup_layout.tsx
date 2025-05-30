6f6e57238717691a0662bc1d4a8025dd921e57f4
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-30 20:45:33 +0700

    add toggle button

--- src/screens/login_signup_layout.tsx
+++ src/screens/login_signup_layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router'
 import { ToastContainer, Bounce } from 'react-toastify';
+import ToggleMode from '../components/toggle_mode';
 
 function LoginSignupLayout() {
     return(
@@ -7,8 +8,9 @@ function LoginSignupLayout() {
             <div className={`max-w-[1620px] max-h-[1040px] h-full w-full bg-background flex flex-col`}>
                 
                 {/* Header */}
-                <div className={`w-full min-h-14 h-14 bg-accent border-b-1 border-greyist px-6 lg:px-20 flex items-center`}>
+                <div className={`w-full min-h-14 h-14 bg-accent border-b-1 border-accent-2 px-6 lg:px-20 flex items-center justify-between`}>
                     <h1 className={`text-xl text-primary font-semibold`}>HCIA Practice</h1>
+                    <ToggleMode />
                 </div>
 
                 <div className={`w-full h-full px-6 lg:px-20 grid grid-cols-1 lg:grid-cols-2 lg:space-x-20`}>
