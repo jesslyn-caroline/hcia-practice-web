f8742aef7751db78dcf309c408d5fd564f11217f
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-31 22:38:25 +0700

    new component

---
+++ src/components/action_button.tsx
@@ -0,0 +1,20 @@
+interface Props {
+    action: () => void,
+    text: string,
+    icon: string,
+    isOnLoad: boolean
+}
+
+function ActionButton({action, text, icon, isOnLoad} : Props) {
+    return (
+        <button className={`h-fit bg-primary px-4 py-2 rounded-md text-white relative flex justify-center cursor-pointer hover:bg-[#AF0009] transition-all`} onClick={action}>
+            <div className={`${isOnLoad ? "opacity-0" : "opacity-100"} flex flex-row place-items-center space-x-2`}>
+                <i className={`${icon} text-xl`} />
+                <span className={`font-medium`}>{text}</span>
+            </div>
+            <i className={`${isOnLoad ? "opacity-100" : "opacity-0"} ri-loader-line animate-spin text-xl origin-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2`}/>
+        </button>
+    )
+}
+
+export default ActionButton
