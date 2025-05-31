f1519faf0ac85cc9747213368a2aec30ad1e32e1
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-01 00:50:10 +0700

    fix: neater styling and smoother effect

--- src/screens/layout.tsx
+++ src/screens/layout.tsx
@@ -1,46 +1,74 @@
 import { useState } from "react"
 import { Link, Outlet } from "react-router"
 
+interface RouteObject {
+    link: string
+    name: string
+    icon: string
+}
+
 function Layout() {
-    const [isClosedSideBar, setIsClosedSideBar] = useState<boolean>(true)
+    const [isOpenedSideBar, setIsOpenedSideBar] = useState<boolean>(false)
 
     function toggleSideBar(event: string) {
-        if (event === "close") setIsClosedSideBar(true)
-        else setIsClosedSideBar(false)
+        if (event === "close") setIsOpenedSideBar(false)
+        else setIsOpenedSideBar(true)
+    }
+
+    const adminRoute: RouteObject[] = [
+        {link: "/", name: "Home", icon: "ri-home-2-line"},
+        {link: "/create-question", name: "Create Question", icon: "ri-list-check-2"},
+    ]
+    // const userRoute: RouteObject[] = [
+    //     {link: "/", name: "Home"},
+    // ]
+
+    const [currentActiveRoute, setCurrentActiveRoute] = useState<string>(() => {
+        const currentActiveRoute = sessionStorage.getItem("currentActiveRoute") || "/"
+        return currentActiveRoute
+    })
+
+    function changeRoute(route: string) {
+        setCurrentActiveRoute(route)
+        sessionStorage.setItem("currentActiveRoute", route)
+
+        toggleSideBar("close")
     }
 
     return(
         <div className={`w-full flex justify-center`}>
-            <div className={`max-w-[1620px] w-full h-full bg-background`}>
+            <div className={`max-w-[1620px] w-full h-screen bg-background`}>
                 {/* Overlay */}
-                <div className={`${isClosedSideBar? "hidden" : ""} md:hidden absolute w-full h-full bg-black opacity-50 z-10`} />
+                <div className={`${isOpenedSideBar? "" : "hidden"} md:hidden absolute w-full h-full bg-black opacity-50 z-10 transition-all`} />
 
-                {/* Sidebar */}
-                <div className={`${isClosedSideBar? "hidden" : ""} absolute bg-background z-20 min-w-xs w-1/4 h-full transition-all`}>
+                {/* SideBar */}
+                <div className={`${isOpenedSideBar? "translate-x-0" : "-translate-x-full"} z-20 absolute left-0 w-2xs h-full bg-background ease-in-out duration-200 border-r-1 border-accent-2`}>
                     <div className={`h-14 px-9 flex items-center`}>
                         <i className={`text-2xl ri-menu-fold-line`} onClick={() => toggleSideBar("close")}/>
                     </div>
-                    <div className={`px-6 py-2 flex flex-col space-y-2 `}>
-                        <Link to="/" className={`flex flex-row items-center space-x-4 px-3 py-2 hover:font-semibold hover:bg-gray-100 rounded-md transition-all`}>
-                            <i className={`font-normal text-2xl ri-home-5-line`} />
-                            <span className={`text-md`}>Home</span>
-                        </Link>
-                        <Link to="/create-question" className={`flex flex-row items-center space-x-4 px-3 py-2 hover:font-semibold hover:bg-gray-100 rounded-md transition-all`}>
-                            <i className={`font-normal text-2xl ri-survey-line`}/>
-                            <span className={`text-sm`}>Create question</span>
-                        </Link>
+                    <div className={`px-6 py-2 flex flex-col space-y-3 `}>
+                        { 
+                             ...adminRoute.map((route) => {
+                                 return (
+                                <Link to={route.link} className={`flex flex-row items-center space-x-4 px-3 py-2 ${currentActiveRoute === route.link? "bg-gray-100 font-semibold" : ""} hover:bg-gray-100 rounded-md transition-colors`} onClick={() => changeRoute(route.link)}>
+                                    <i className={`font-normal text-2xl ${route.icon}`}/>
+                                    <span className={`text-sm`}>{route.name}</span>
+                                </Link>
+                                )
+                            })
+                        }
                     </div>
                 </div>
 
                 {/* Content */}
-                <div className={`flex flex-row`}>
-                    <div className={`${isClosedSideBar? "hidden" : "md:block"} hidden min-w-xs w-1/4`} />
-                    <div className={`w-full`}>
-                        <div className={`w-full h-14 px-10 bg-primary flex items-center`}>
-                            <i className={`text-2xl text-white ri-menu-line`} onClick={() => toggleSideBar("open")}/>
+                <div className={`w-full flex flex-row`}>
+                    <div className={`hidden ${isOpenedSideBar? "md:block" : "hidden"} min-w-2xs w-2xs transition-discrete`} />
+                    <div className={`w-full h-full flex flex-col`}>
+                        <div className={`h-14 px-10 bg-primary flex items-center`}>
+                            <i className={`${isOpenedSideBar? "opacity-0" : "opacity-100"} text-2xl text-white ri-menu-line`} onClick={() => toggleSideBar("open")}/>
                         </div>
                         <div className={`px-10 py-6`}>
-                            <Outlet />
+                           <Outlet />
                         </div>
                     </div>
                 </div>
