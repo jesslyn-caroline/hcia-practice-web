eb83fd7a63330522fb767e741c1f400e656fe936
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-01 20:48:03 +0700

    divide userContext into LoginContext and userContext

---
+++ src/provider/login_context.tsx
@@ -0,0 +1,102 @@
+import { createContext, useContext, useState } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router"
+
+import { ErrorMessageContext } from "./error_message_context"
+import toast_error from "../components/toast/toast_error"
+import toast_success from "../components/toast/toast_success"
+import { UserContext } from "./user_context"
+
+export const LoginContext = createContext({
+    userId: "",
+    password: "", 
+    isOnLoad: false,
+
+    handleUserIdChange: (e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value) },
+    handlePasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value) },
+    login: () => {},
+    clearInputs: () => {}
+})
+
+function LoginProvider({children} : {children : React.ReactNode}) {
+    const navigate = useNavigate()
+
+    const { setUserId, setUsername, setStudentClass, setRole } = useContext(UserContext)
+    const { setUserIdErrMessage, setPasswordErrMessage } = useContext(ErrorMessageContext)
+
+    const [userId, setUserIdVal] = useState<string>("")
+    const handleUserIdChange = (e: React.ChangeEvent<HTMLInputElement>):void => setUserIdVal(e.target.value)
+
+    const [password, setPassword] = useState<string>("")
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>): void => setPassword(e.target.value)
+
+    function validation():boolean {
+        let valid:boolean = true
+
+        if (userId === "") {
+            setUserIdErrMessage("User ID is required")
+            valid = false
+        }
+        else setUserIdErrMessage("")
+
+        if (password.length === 0) {
+            setPasswordErrMessage("Password is required")
+            valid = false
+        }
+        else setPasswordErrMessage("")
+
+        return valid
+    }
+
+    const [isOnLoad, setIsOnLoad] = useState<boolean>(false)
+    async function login():Promise<void> {
+        let valid:boolean = validation()
+
+        if (!valid) return
+
+        setIsOnLoad(true)
+
+        try {
+            const response = await axios.post(`https://huawei-practice-web-backend.vercel.app/api/user/login`, {userId, password})
+
+            if (response.status === 200) {
+                setUserId(response.data.user.userId)
+                setUsername(response.data.user.username)
+                setStudentClass(response.data.user.class)
+                setRole(response.data.user.role)
+
+                sessionStorage.setItem("userId", response.data.user.userId)
+                sessionStorage.setItem("username", response.data.user.username)
+                sessionStorage.setItem("class", response.data.user.class)
+                sessionStorage.setItem("role", response.data.user.role)
+
+                clearInputs()
+
+                toast_success(response.data.message)
+                
+                setTimeout(() => {
+                    navigate("/")
+                    setPassword("")
+                }, 3000)
+            }
+        }
+        catch (err:any) {
+            toast_error(err.response.data.message)
+        }
+
+        setIsOnLoad(false)
+    }
+
+    function clearInputs():void {
+        setUserId("")
+        setPassword("")
+    } 
+
+    return (
+        <LoginContext.Provider value={{userId, password, isOnLoad, handleUserIdChange, handlePasswordChange, login, clearInputs}}>
+            {children}
+        </LoginContext.Provider>
+    )
+}
+
+export default LoginProvider
--- src/provider/user_context.tsx
+++ src/provider/user_context.tsx
@@ -1,107 +1,29 @@
-import { createContext, useContext, useState } from "react"
-import axios from "axios"
-import { useNavigate } from "react-router"
-
-import { ErrorMessageContext } from "./error_message_context"
-import toast_error from "../components/toast/toast_error"
-import toast_success from "../components/toast/toast_success"
+import { createContext, useState } from "react";
 
 export const UserContext = createContext({
     userId: "",
-    password: "", 
     username: "",
     studentClass: "",
     role: "",
-    isOnLoad: false,
 
-    handleUserIdChange: (e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value) },
-    handlePasswordChange: (e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value) },
-    login: () => {}
+    setUserId: (userId: string) => { console.log(userId) },
+    setUsername: (username: string) => { console.log(username) },
+    setStudentClass: (studentClass: string) => { console.log(studentClass) },
+    setRole: (role: string) => { console.log(role) },
 })
 
-function UserProvider({children} : {children : React.ReactNode}) {
-    const navigate = useNavigate()
-
-    const { setUserIdErrMessage, setPasswordErrMessage } = useContext(ErrorMessageContext)
-
+function UserProvider({children}: {children: React.ReactNode}) {
     const [userId, setUserId] = useState<string>(getDataFromSession("userId"))
-
-    const [password, setPassword] = useState<string>("")
-
     const [username, setUsername] = useState<string>(getDataFromSession("username"))
     const [studentClass, setStudentClass] = useState<string>(getDataFromSession("class"))
     const [role, setRole] = useState<string>(getDataFromSession("role"))
-
-    function handleUserIdChange(e: React.ChangeEvent<HTMLInputElement>):void {
-        setUserId(e.target.value)
-    }
-
-    function handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>):void {
-        setPassword(e.target.value)
-    }
 
     function getDataFromSession(data: string):string {
         return sessionStorage.getItem(data) || ""
     }
 
-    function validation():boolean {
-        let valid:boolean = true
-
-        if (userId === "") {
-            setUserIdErrMessage("User ID is required")
-            valid = false
-        }
-        else setUserIdErrMessage("")
-
-        if (password.length === 0) {
-            setPasswordErrMessage("Password is required")
-            valid = false
-        }
-        else setPasswordErrMessage("")
-
-        return valid
-    }
-
-    const [isOnLoad, setIsOnLoad] = useState<boolean>(false)
-    async function login():Promise<void> {
-        let valid:boolean = validation()
-
-        if (!valid) return
-
-        setIsOnLoad(true)
-
-        try {
-            const response = await axios.post(`https://huawei-practice-web-backend.vercel.app/api/user/login`, {userId, password})
-            
-            console.log(response.data.user)
-
-            if (response.status === 200) {
-                setUsername(response.data.user.username)
-                setStudentClass(response.data.user.class)
-                setRole(response.data.user.role)
-
-                sessionStorage.setItem("userId", response.data.user.userId)
-                sessionStorage.setItem("username", response.data.user.username)
-                sessionStorage.setItem("class", response.data.user.class)
-                sessionStorage.setItem("role", response.data.user.role)
-
-                toast_success(response.data.message)
-                
-                setTimeout(() => {
-                    navigate("/")
-                    setPassword("")
-                }, 3000)
-            }
-        }
-        catch (err:any) {
-            toast_error(err.response.data.message)
-        }
-
-        setIsOnLoad(false)
-    }
-
     return (
-        <UserContext.Provider value={{userId, username, password, studentClass, role, isOnLoad, handleUserIdChange, handlePasswordChange, login}}>
+        <UserContext.Provider value={{userId, username, studentClass, role, setUserId, setUsername, setStudentClass, setRole}}>
             {children}
         </UserContext.Provider>
     )
