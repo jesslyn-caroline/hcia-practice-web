68e2db854bd576ae1ff700be2c34a93eb33e19f8
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-02 00:30:02 +0700

    add new route "/question-list" for admin

--- src/provider/user_context.tsx
+++ src/provider/user_context.tsx
@@ -30,6 +30,7 @@ function UserProvider({children}: {children: React.ReactNode}) {
     const adminRoute: RouteObject[] = [
         {link: "/", name: "Home", icon: "ri-home-2-line"},
         {link: "/create-question", name: "Create Question", icon: "ri-list-check-2"},
+        {link: "/question-list", name: "Question List", icon: "ri-list-check-2"},
     ]
     const userRoute: RouteObject[] = [
         {link: "/", name: "Home", icon: "ri-home-2-line"},
