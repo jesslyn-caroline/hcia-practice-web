c8b89f8df0c190e37e5d1aa9abc684daa6af1e34
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-02 00:29:33 +0700

    new page

---
+++ src/screens/question_list.tsx
@@ -0,0 +1,48 @@
+import { useContext } from "react"
+
+import { QuestionListContext } from "../provider/question_list_context"
+
+function QuestionList() {
+
+    const { questionList, deleteQuestion } = useContext(QuestionListContext)
+
+    return (
+        <div>
+            <div className={`flex flex-col`}>
+                <i className={`text-4xl ri-list-check-2 mb-2`}/>
+                <h1 className={`text-xl font-semibold mb-8`}>Question List</h1>
+                <div className={`overflow-x-auto`}>
+                    <table className="min-w-full divide-y divide-gray-200">
+                        <thead className="text-gray-500 uppercase">
+                            <tr>
+                                <th scope="col" className="px-6 py-3 font-medium">No.</th>
+                                <th scope="col" className="px-6 py-3 font-medium text-start">Question</th>
+                                <th scope="col" className="px-6 py-3 font-medium">Year</th>
+                                <th scope="col" className="px-6 py-3 font-medium">Type</th>
+                                <th scope="col" className="px-6 py-3 font-medium text-start">Answer</th>
+                                <th scope="col" className="px-6 py-3 font-medium">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-200">
+                            { ...questionList.map((question, index) => {
+                                return (
+                                    <tr key={question._id}>
+                                        <td className="px-6 py-4 text-center">{index + 1}</td>
+                                        <td className="px-6 py-4">{question.question}</td>
+                                        <td className="px-6 py-4">{question.year}</td>
+                                        <td className="px-6 py-4">{question.type.split("-").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")}</td>
+                                        <td className="px-6 py-4">{question.answer}</td>
+                                        <td className="px-6 py-4"><i className={`ri-delete-bin-2-line`} onClick={() => deleteQuestion(question._id)}/></td>
+                                    </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>       
+            </div>
+        </div>
+    )
+}
+
+export default QuestionList
