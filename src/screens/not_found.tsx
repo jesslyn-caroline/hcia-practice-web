352358af9c729ac0d2cef3129664bbb23adafb8b
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-02 22:03:47 +0700

    handle bug if user doesnt have a role

--- src/screens/not_found.tsx
+++ src/screens/not_found.tsx
@@ -1,9 +1,13 @@
 import { useNavigate } from "react-router"
+import { useContext, useState } from "react"
+
 import ToggleMode from "../components/toggle_mode"
-import { useState } from "react"
+import { UserContext } from "../provider/user_context"
 
 function NotFound() {
     const navigate = useNavigate()
+
+    const { role } = useContext(UserContext)
 
     const [second, setSecond] = useState<number>(5)
 
@@ -12,7 +16,8 @@ function NotFound() {
     }, 1000)
 
     setTimeout(() => {
-        navigate("/")
+        if (role === "") navigate("/login")
+        else navigate("/")
     }, 6000)
 
     return (
@@ -24,7 +29,7 @@ function NotFound() {
                 </div>
                 <div className={`w-full h-full place-items-center`}>
                     <img src="/images/404-error-rafiki.png" alt="" className={`w-sm`}/>
-                    <h1 className={`-mt-5 text-md font-semibold`}>We will send you back to the Home in {second} s</h1>
+                    <h1 className={`-mt-5 text-md font-semibold`}>We will send you back to the {role === ""? "Login" : "Home"} in {second} s</h1>
                 </div>
             </div>
         </div>
