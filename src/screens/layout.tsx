fafca5a333820c7267bfa4d3c98f8106b0cd5d01
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-02 22:41:51 +0700

    update: close bar if user tap overlay

--- src/screens/layout.tsx
+++ src/screens/layout.tsx
@@ -18,10 +18,10 @@ function Layout() {
         <div className={`w-full flex justify-center`}>
             <div className={`max-w-[1620px] w-full h-screen bg-background`}>
                 {/* Overlay */}
-                <div className={`${isOpenedSideBar? "" : "hidden"} md:hidden absolute w-full h-full bg-black opacity-50 z-10 transition-all`} />
+                <div className={`${isOpenedSideBar? "" : "hidden"} md:hidden absolute w-full h-full bg-black opacity-50 z-10 transition-all`} onClick={() => toggleSideBar("close")}/>
 
                 {/* SideBar */}
-                <div className={`${isOpenedSideBar? "translate-x-0" : "-translate-x-full"} z-20 absolute left-0 w-2xs h-full bg-background ease-in-out duration-200 border-r-1 border-accent-2`}>
+                <div className={`${isOpenedSideBar? "translate-x-0" : "-translate-x-full"} z-20 absolute left-0 w-2xs h-full bg-background ease-in-out duration-200`}>
                     <div className={`h-14 px-7 md:px-9 flex items-center`}>
                         <i className={`text-2xl ri-menu-fold-line cursor-pointer`} onClick={() => toggleSideBar("close")}/>
                     </div>
@@ -29,7 +29,7 @@ function Layout() {
                         { 
                             ...userRoutes.map((route) => {
                                  return (
-                                <Link to={route.link} className={`flex flex-row items-center space-x-4 px-3 py-2 ${currentActiveRoute === route.link? "bg-gray-100 font-semibold" : ""} hover:bg-gray-100 rounded-md transition-colors`} onClick={() => {}}>
+                                <Link to={route.link} className={`flex flex-row items-center space-x-4 px-3 py-2 ${currentActiveRoute === route.link? "bg-gray-100 font-semibold" : ""} hover:bg-gray-100 rounded-md transition-colors`}>
                                     <i className={`font-normal text-2xl ${route.icon}`}/>
                                     <span className={`text-sm`}>{route.name}</span>
                                 </Link>
@@ -40,10 +40,10 @@ function Layout() {
                 </div>
 
                 {/* Content */}
-                <div className={`w-full flex flex-row`}>
+                <div className={`w-full h-full flex flex-row`}>
                     <div className={`hidden ${isOpenedSideBar? "md:block" : "hidden"} min-w-2xs w-2xs transition-discrete`} />
-                    <div className={`w-full h-full flex flex-col`}>
-                        <div className={`h-14 px-4 md:px-10 bg-primary flex items-center`}>
+                    <div className={`min-h-screen w-full h-fit border-l-1 border-accent-2 flex flex-col`}>
+                        <div className={`min-h-14 h-14 px-4 md:px-10 bg-primary flex items-center`}>
                             <i className={`${isOpenedSideBar? "opacity-0" : "opacity-100"} text-2xl text-white ri-menu-line cursor-pointer`} onClick={() => toggleSideBar("open")}/>
                         </div>
                         <div className={`px-4 md:px-10 py-6`}>
