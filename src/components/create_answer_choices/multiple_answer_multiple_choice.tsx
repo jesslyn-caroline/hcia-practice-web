3c063bba43ce3ee2311c3d61127fb60a02957d22
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-06 18:16:41 +0700

    fix: remove React

--- src/components/create_answer_choices/multiple_answer_multiple_choice.tsx
+++ src/components/create_answer_choices/multiple_answer_multiple_choice.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import { useContext } from "react"
 
 import { CreateQuestionContext } from "../../provider/create_question_context"
 import { EditQuestionContext } from "../../provider/edit_question_context"
