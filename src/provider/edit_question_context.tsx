be4e9dcdba531ad85e2b60608d54329642ca5e16
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-06 15:49:14 +0700

    new feature

---
+++ src/provider/edit_question_context.tsx
@@ -0,0 +1,270 @@
+import { useState, createContext, useContext, useEffect } from "react"
+import axios from "axios"
+
+import { ErrorMessageContext } from "./error_message_context"
+import toast_success from "../components/toast/toast_success"
+import toast_error from "../components/toast/toast_error"
+import { UserContext } from "./user_context"
+import { useNavigate, useParams } from "react-router"
+
+export const EditQuestionContext = createContext({
+    year : "",
+    typeOptions: ["", "", "", ""],
+    type : "",
+    score : 0,
+    question : "",
+    optionsValue : ["", "", "", ""],
+    isOptionsSelected : [false, false, false, false],
+    isOnLoad : false,
+
+    handleTypeChange : (e: React.ChangeEvent<HTMLSelectElement>) => { console.log(e.target.value) },
+    handleYearChange : (e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value) },
+    handleScoreChange : (e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value) },
+    handleQuestionChange : (e: React.ChangeEvent<HTMLTextAreaElement>) => { console.log(e.target.value) },
+    handleOptionValueChange : (index:number, e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value, index) },
+    handleIsOptionsSelectedChange : (index:number, e: React.ChangeEvent<HTMLInputElement>) => { console.log(e.target.value, index) },
+
+    saveQuestion: () => { console.log("save") },
+    clearInputs: () => { console.log("clearInputs") }
+})
+
+interface QuestionModel {
+    _id: string,
+    question: string,
+    year: number,
+    type: string,
+    options: string[],
+    answer: string[],
+    score: number
+}
+
+function EditQuestionProvider ({children} : {children : React.ReactNode}) {
+
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const { setQuestionErrMessage, setYearErrMessage, setScoreErrMessage, setOptionsErrMessage, setNoAnswerErrMessage } = useContext(ErrorMessageContext)
+    const { currentActiveRoute } = useContext(UserContext)
+
+    const typeOptions: string[] = ["multiple-answer-multiple-choice", "single-answer-multiple-choice", "true-or-false", "single-word-answer"]
+    const [type, setType] = useState<string>("multiple-answer-multiple-choice")
+    const handleTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setType(e.target.value)
+
+        // reset options value and isSelected
+        setOptionsValue(["", "", "", ""])
+        setIsOptionsSelected([false, false, false, false])
+
+        if (e.target.value === "single-word-answer") setIsOptionsSelected([true, false, false, false])
+        
+        if (e.target.value === "true-or-false") setOptionsValue(["True", "False", "", ""])
+    }
+
+    const date = new Date()
+    const [year, setYear] = useState<string>(date.getFullYear().toString())
+    const handleYearChange = (e: React.ChangeEvent<HTMLInputElement>):void => setYear(e.target.value)
+
+    const [score, setScore] = useState<number>()
+    const handleScoreChange = (e: React.ChangeEvent<HTMLInputElement>):void => setScore(parseInt(e.target.value))
+
+    const [question, setQuestion] = useState<string>("")
+    const handleQuestionChange = (e: React.ChangeEvent<HTMLTextAreaElement>):void => setQuestion(e.target.value)
+    
+    const [optionsValue, setOptionsValue] = useState<string[]>(["", "", "", ""])
+    const handleOptionValueChange = (index:number, e: React.ChangeEvent<HTMLInputElement>):void => {
+        let newOptionsValue = [...optionsValue]
+        newOptionsValue[index] = e.target.value
+
+        setOptionsValue(newOptionsValue)
+    }
+
+
+    const [isOptionsSelected, setIsOptionsSelected] = useState<boolean[]>([false, false, false, false])
+
+    // only for multiple-answer-multiple-choice && single-answer-multiple-choice && true-or-false
+    // bcs single-word-answer has only one option
+    const handleIsOptionsSelectedChange = (index:number, e: React.ChangeEvent<HTMLInputElement>):void => {
+        let newIsOptionsSelected = [...isOptionsSelected]
+
+        if (type === "multiple-answer-multiple-choice") {
+            newIsOptionsSelected[index] = e.target.checked
+        }
+        else {
+            for (let i = 0; i < 4; i++) {
+                if (i === index) newIsOptionsSelected[i] = true
+                else newIsOptionsSelected[i] = false
+            }
+        }
+
+        setIsOptionsSelected(newIsOptionsSelected)
+    }
+
+    function validation ():boolean {
+        let valid:boolean = true
+
+        if (question === "") {
+            setQuestionErrMessage("Please fill the question")
+            valid = false
+        }
+        else setQuestionErrMessage("")
+
+        if (year === "") {
+            setYearErrMessage("Please fill the year")
+            valid = false
+        }
+        else if (year.length !== 4) {
+            setYearErrMessage("Please enter valid year")
+            valid = false
+        }
+        else if (year[0].toUpperCase() !== year[0].toLowerCase() ||
+            year[1].toUpperCase() !== year[1].toLowerCase() ||
+            year[2].toUpperCase() !== year[2].toLowerCase() ||
+            year[3].toUpperCase() !== year[3].toLowerCase()) {
+            setYearErrMessage("Year must be a number")
+            valid = false
+        }
+        else setYearErrMessage("")
+
+
+        if (isNaN(score!) || score === undefined) {
+            setScoreErrMessage("Please fill the score")
+            valid = false
+        }
+        else if (score < 0) {
+            setScoreErrMessage("Score may not be negative")
+            valid = false
+        }
+        else setScoreErrMessage("")
+
+        if (question === "") {
+            setQuestionErrMessage("Please fill the question")
+            valid = false
+        }
+        else setQuestionErrMessage("")
+
+        let optionsErrMessage:string[] = ["", "", "", ""]
+        let isNoAnswer = true
+
+        if (type === "single-word-answer") {
+            isNoAnswer = false
+            if (optionsValue[0] === "") {
+                optionsErrMessage[0] = "Please enter the question"
+                valid = false
+            }
+            else optionsErrMessage[0] = ""
+        }
+        else if (type === "true-or-false") {
+            if (!isOptionsSelected[0] && !isOptionsSelected[1]) {
+                isNoAnswer = true
+                valid = false
+            }
+            else isNoAnswer = false
+        }
+        else {
+            for (let i = 0; i < 4; i++) {
+                if (optionsValue[i] === "") {
+                    optionsErrMessage[i] = "Please fill the option"
+                    valid = false
+                }
+                else optionsErrMessage[i] = ""
+    
+                if (isOptionsSelected[i]) isNoAnswer = false
+            }
+        }
+
+        setOptionsErrMessage(optionsErrMessage)
+
+        if (isNoAnswer) {
+            setNoAnswerErrMessage("Please select the answer")
+            valid = false
+        }
+        else setNoAnswerErrMessage("")
+
+        return valid
+    }
+
+    const [isOnLoad, setIsOnLoad] = useState<boolean>(false)
+
+    async function saveQuestion() {
+        let valid:boolean = validation()
+
+        if (!valid) return
+
+        setIsOnLoad(true)
+
+        let answer:string[] = []
+
+        for (let i = 0; i < 4; i++) {
+            if (isOptionsSelected[i]) answer.push(optionsValue[i])
+        }
+
+        try {
+            const response = await axios.put(`https://huawei-practice-web-backend.vercel.app/api/question/${id}`, 
+                {question, year: parseInt(year), type, score, answer, options: optionsValue}
+            )
+            
+            if (response.status === 200) {
+                toast_success(response.data.message)
+                navigate(`/question-list`)
+            }
+        }
+        catch (err: any) {
+            toast_error(err.response.data.message)
+        }
+
+        setIsOnLoad(false)
+    }
+
+    const clearInputs = () => {
+        setYear(date.getFullYear().toString())
+        setType("multiple-answer-multiple-choice")
+        setScore(0)
+        setQuestion("")
+        setOptionsValue(["", "", "", ""])
+        setIsOptionsSelected([false, false, false, false])
+    }
+
+    useEffect(() => {
+        clearInputs()
+    }, [currentActiveRoute])
+
+    async function getQuestion() {
+        try {
+            const response = await axios.get(`https://huawei-practice-web-backend.vercel.app/api/question/${id}`)
+            
+            if (response.status === 200) {
+                const question:QuestionModel = response.data
+                setYear(question.year.toString())
+                setType(question.type)
+                setScore(question.score)
+                setQuestion(question.question)
+                setOptionsValue(question.options)
+
+                let selectedOption = [false, false, false, false]
+                for (let i = 0; i < 4; i++) {
+                    for (let j = 0; j < question.answer.length; j++) {
+                        if (question.options[i] === question.answer[j]) {
+                            selectedOption[i] = true
+                        }
+                    }
+                }
+                setIsOptionsSelected(selectedOption)
+            }
+        }
+        catch (err: any) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getQuestion()
+    }, [id])
+
+    return (
+        <EditQuestionContext.Provider value={{year, typeOptions, type, score: score !== undefined ? score : 0, question, optionsValue, isOptionsSelected, isOnLoad, handleTypeChange, handleYearChange, handleScoreChange, handleQuestionChange, handleOptionValueChange, handleIsOptionsSelectedChange, saveQuestion, clearInputs}}>
+            { children }
+        </EditQuestionContext.Provider>
+    )
+}
+
+export default EditQuestionProvider
---
+++ src/screens/edit_question.tsx
@@ -0,0 +1,72 @@
+import { useContext } from "react"
+
+import MultipleAnswerMultipleChoice from "../components/create_answer_choices/multiple_answer_multiple_choice"
+import { EditQuestionContext } from "../provider/edit_question_context"
+import SingleAnswerMultipleChoice from "../components/create_answer_choices/single_answer_multiple_choice"
+import SingleWordAnswer from "../components/create_answer_choices/single_word_answer"
+import TrueOrFalse from "../components/create_answer_choices/true-or-false"
+import { ErrorMessageContext } from "../provider/error_message_context"
+import InputField from "../components/field/input_field"
+import SelectField from "../components/field/select_field"
+import ActionButton from "../components/action_button"
+
+
+function EditQuestion() {
+
+    const { year, score, typeOptions, type, question, isOnLoad, handleYearChange, handleTypeChange, handleScoreChange, handleQuestionChange, saveQuestion } = useContext(EditQuestionContext)
+
+    const { yearErrMessage, scoreErrMessage, questionErrMessage } = useContext(ErrorMessageContext)
+
+    return (
+        <div className={`flex flex-col`}>
+            <i className={`text-4xl ri-menu-add-line mb-2`}/>
+            <h1 className={`text-xl font-semibold mb-8`}>Create Question</h1>
+            <div className={`space-y-8`}>
+                <div className={`flex flex-col md:flex-row md:space-x-12`}>
+                    <div>
+                        <SelectField handleSelectChange={handleTypeChange} 
+                            optionsValue={typeOptions} 
+                            optionsLabel={["Multiple Answers Multiple Choice", "Single Answer Multiple Choice", "True or False", "Single Word Answer"]}
+                            labelValue="Question Type" 
+                            titleValue="question-type"
+                            errMessage=""
+                            value={type} />
+                    </div>
+                    <div className={`flex flex-row space-x-12`}>
+                        <InputField handleInputChange={handleYearChange}
+                            inputType="text" 
+                            errMessage={yearErrMessage} 
+                            placeholderValue="YYYY" 
+                            idValue="question-year" 
+                            labelValue="Year"
+                            value={year} />
+                        <InputField handleInputChange={handleScoreChange} 
+                            inputType="number" 
+                            errMessage={scoreErrMessage} 
+                            placeholderValue="Score" 
+                            idValue="question-score" 
+                            labelValue="Score" 
+                            value={score} />
+                    </div>
+                </div>
+
+                <div className={`relative`}>
+                    <div className={`font-semibold`}>Question Type</div>
+                    <textarea value={question} onChange={handleQuestionChange} rows={4} className={`w-full mt-2 resize-none outline-none p-2 border-2 border-accent-2 rounded-sm`} id="question" placeholder="Enter your question"></textarea>
+                    <div className={`${questionErrMessage === "" ? "hidden" : ""} absolute text-xs text-red-500 mt-1`}>{questionErrMessage}</div>
+                </div>
+
+                <div>
+                    { type === "multiple-answer-multiple-choice"? <MultipleAnswerMultipleChoice mode="edit"/> : null }
+                    { type === "single-answer-multiple-choice"? <SingleAnswerMultipleChoice mode="edit"/> : null }
+                    { type === "single-word-answer"? <SingleWordAnswer mode="edit"/> : null }
+                    { type === "true-or-false"? <TrueOrFalse mode="edit"/> : null }
+                </div>
+                <ActionButton action={saveQuestion} 
+                    text={"Save"} icon={"ri-save-fill"} isOnLoad={isOnLoad} />
+            </div>
+        </div>
+    )
+}
+
+export default EditQuestion
