acc03eb888ea70b507d4a10fd46b041e31fb9ea1
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-09 00:14:07 +0700

    update content

--- src/screens/class_list.tsx
+++ src/screens/class_list.tsx
@@ -1,10 +1,23 @@
+import { useContext } from "react"
+import { ClassListContext } from "../provider/class_list_context"
+import ClassCard from "../components/class_card"
 
 
 function ClassList() {
+    const {classList, viewClass} = useContext(ClassListContext)
+
     return (
         <div className={`flex flex-col`}>
             <i className={`text-4xl ri-group-3-line mb-2`}/>
             <h1 className={`text-xl font-semibold mb-8`}>Classes</h1>
+
+            {
+                ...classList.map((value) => { 
+                    return (
+                        <ClassCard name={value.name} academicYear={value.academicYear} classId={value._id} viewClass={viewClass}/>
+                    )
+                })
+            }
         </div>
     )
 }
