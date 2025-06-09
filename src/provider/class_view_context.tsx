123c8b04a78c7232829085ee1d1d2f01fafec6fb
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-09 23:24:22 +0700

    add getMembers

--- src/provider/class_view_context.tsx
+++ src/provider/class_view_context.tsx
@@ -1,19 +1,38 @@
 import { createContext } from "react";
 import { useParams } from "react-router";
+import type { UserModel } from "../model/user_model";
+import axios from "axios";
+import toast_error from "../components/toast/toast_error";
 
 
 export const ClassViewContext = createContext({
-    id : ""
+    id : "",
+    members : [{userId: "", username: "", classId: "", role: ""}]
 })
 
-function ClassViewProvider ({children} : {children : React.ReactNode}) {
+async function ClassViewProvider ({children} : {children : React.ReactNode}) {
 
     const { id } = useParams()
 
-    // const [members, setMembers] = useState<string>()
+    const members:UserModel[] = await getMembers()
+
+    
+    async function getMembers():Promise<UserModel[]> {
+        try {
+            const response = await axios.get(`https://huawei-practice-web-backend.vercel.app/api/user?classId=${id}`)
+
+            if (response.status === 200) {
+                return response.data
+            }
+        }
+        catch (err: any) {
+            toast_error(err.response.data.message)
+        }
+        return []
+    }
 
     return (
-        <ClassViewContext.Provider value={{id : (id === undefined ? "" : id)}}>
+        <ClassViewContext.Provider value={{id : (id === undefined ? "" : id), members}}>
             {children}
         </ClassViewContext.Provider>
     )
