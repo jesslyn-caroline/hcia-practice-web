d4963cba0fe956980b767b557331b0c73d96f1ae
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-16 23:48:48 +0700

    flex wrap for cards

--- src/screens/class_list.tsx
+++ src/screens/class_list.tsx
@@ -1,16 +1,20 @@
 import { useContext } from "react"
 import { ClassListContext } from "../provider/class_list_context"
 import ClassCard from "../components/class_card"
+import ActionButton from "../components/action_button"
 
 
 function ClassList() {
-    const {classList, viewClass} = useContext(ClassListContext)
+    const {classList, viewClass, newClass} = useContext(ClassListContext)
 
     return (
         <div className={`flex flex-col`}>
             <i className={`text-4xl ri-group-3-line mb-2`}/>
-            <h1 className={`text-xl font-semibold mb-8`}>Classes</h1>
-
+            <h1 className={`text-xl font-semibold mb-6`}>Classes</h1>
+            <div className={`mb-4`}>
+                <ActionButton action={newClass} text="New Class" icon="ri-add-line" />
+            </div>
+            <div className={`flex flex-wrap space-x-4`}>
             {
                 ...classList.map((value) => { 
                     return (
@@ -18,6 +22,9 @@ function ClassList() {
                     )
                 })
             }
+            </div>
+
+            
         </div>
     )
 }
