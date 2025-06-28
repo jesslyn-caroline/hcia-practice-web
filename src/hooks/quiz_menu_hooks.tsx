31a40775642b244089803e4b118f987cd4b1c7ad
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-28 17:42:14 +0700

    update

--- src/hooks/quiz_menu_hooks.tsx
+++ src/hooks/quiz_menu_hooks.tsx
@@ -2,11 +2,21 @@ import { useContext, useEffect, useState } from "react"
 import { UserContext } from "../provider/user_context"
 import axios from "axios"
 
+
+interface HistoryObj {
+    _id: string,
+    quizId: string,
+    score: number,
+    time: number,
+    type: string,
+    title: string
+}
+
 function QuizMenuHooks() {
 
     const { user } = useContext(UserContext)
 
-    const [history, setHistory] = useState([])
+    const [history, setHistory] = useState<HistoryObj[]>([])
 
     useEffect(() => {
         getHistory()
@@ -14,12 +24,46 @@ function QuizMenuHooks() {
 
     async function getHistory():Promise<void> {
         try {
-            const response = axios.get(`https://huawei-practice-web-backend.vercel.app/api/quiz?userId=${`user.userId`}`)
+            const response = await axios.get(`https://huawei-practice-web-backend.vercel.app/api/quiz?userId=${user.userId}`)
+
+            if (response.status === 200) {
+                // setHistory(response.data)
+                console.log(response.data)
+
+                for (let i = 0; i < response.data.length; i++) {
+                    const quiz = await getQuizInfo(response.data[i].quizId)
+                    let quizObj:HistoryObj = {
+                        title : quiz.title,
+                        time : quiz.time,
+                        type : quiz.type,
+                        score : response.data[i].score,
+                        _id : response.data[i]._id,
+                        quizId : response.data[i].quizId
+                    }
+                    setHistory(prev => [...prev, quizObj])
+                }
+            }
         }
         catch (err: any) {
             console.log(err)
         }
     }
+
+    async function getQuizInfo(quizId: string):Promise<any> {
+        try {
+            const response = await axios.get(`https://huawei-practice-web-backend.vercel.app/api/quiz/${quizId}`)
+
+            if (response.status === 200) {
+                // console.log(response.data)
+                return response.data.quiz
+            }
+        }
+        catch (err: any) {
+            console.log(err)
+        }
+    }
+
+    return {history, getQuizInfo}
 }
 
 export default QuizMenuHooks
--- src/screens/quiz_menu.tsx
+++ src/screens/quiz_menu.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router"
 import ActionButton from "../components/action_button"
+import QuizMenuHooks from "../hooks/quiz_menu_hooks"
 
 function QuizMenu() {
 
-    
+    const {history} = QuizMenuHooks()
 
     return (
         <div className={`flex flex-col`}>
@@ -12,7 +13,40 @@ function QuizMenu() {
             <Link to="/quiz/new">
                 <ActionButton action={() => {}} text="Start Quiz" icon="ri-lightbulb-flash-fill" />
             </Link>
-            <h1 className={`text-md font-semibold mt-6`}>Recently Taken</h1>
+            <h1 className={`text-md font-semibold my-6`}>Recently Taken</h1>
+            <table className="min-w-full divide-y divide-gray-200">
+                <thead className="text-gray-500 uppercase">
+                    <tr>
+                        <th scope="col" className="w-12 font-medium">No.</th>
+                        <th scope="col" className="w-60 px-6 py-3 font-medium text-start">Quiz Title</th>
+                        <th scope="col" className="px-6 py-3 font-medium">Time</th>
+                        <th scope="col" className="px-6 py-3 font-medium">Type</th>
+                        <th scope="col" className="font-medium">Score</th>
+                        <th scope="col" className="px-6 py-3"></th>
+                    </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                    {
+                        ...history.map((quiz, index) => { 
+                            return (
+                            <tr>
+                                <td className="px-6 py-3">{index + 1}</td>
+                                <td className="px-6 py-3">{quiz.title}</td>
+                                <td className="px-6 py-3 text-center">{quiz.time}</td>
+                                <td className="px-6 py-3 text-center">{quiz.type}</td>
+                                <td className="px-6 py-3 text-center">{quiz.score}</td>
+                                <td className="px-6 py-3 text-center">
+                                    <Link to={`/quiz/result/${quiz.quizId}`}>
+                                        VIEW
+                                    </Link>
+                                </td>
+                            </tr>
+                            )
+                        })
+                    }
+                    
+                </tbody>
+            </table>
         </div>
     )
 }
