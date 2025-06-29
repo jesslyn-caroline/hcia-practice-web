844c7afcbf0834f35d8343d72b97304280221745
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-29 20:57:06 +0700

    remove score and type import

--- src/screens/quiz_result.tsx
+++ src/screens/quiz_result.tsx
@@ -2,8 +2,7 @@ import QuizResultHooks from "../hooks/quiz_result_hooks"
 
 function QuizResult() {
 
-    const { quizId, score, type } = QuizResultHooks()
-    console.log(score, type)
+    const { quizId } = QuizResultHooks()
 
     return(
         <div>
@@ -11,8 +10,8 @@ function QuizResult() {
                 {/* <i className={`text-4xl ri-list-check-2 mb-2`}/> */}
                 <h1 className={`text-xl font-semibold`}>Question Result</h1>
                 <h2 className={`text-gray-400 font-semibold`}>{ quizId }</h2>
-                <h3>Score: {score}</h3>
-                <h3>Type: {type}</h3>
+                {/* <h3>Score: {score}</h3>
+                <h3>Type: {type}</h3> */}
             </div>
         </div>
     )
