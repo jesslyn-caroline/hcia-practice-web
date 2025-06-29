ceac261fc7fa2835d03f76ff382271b29348455d
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-06-29 21:56:41 +0700

    update

--- src/hooks/quiz_result_hooks.tsx
+++ src/hooks/quiz_result_hooks.tsx
@@ -1,28 +1,28 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router"
+import { UserContext } from "../provider/user_context"
+import type { QuestionModel } from "../model/question_model"
 
 function QuizResultHooks() {
 
     const { quizId } = useParams()
+    const { user } = useContext(UserContext)
 
     useEffect(() => {
         getHistory()
+        getQuizInfo()
     }, [])
 
-    // const [score, setScore] = useState<number>(0)
-    // const [type, setType] = useState<string>("")
-
+    const [score, setScore] = useState<number>(0)
+    const [questions, setQuestions] = useState<QuestionModel[]>([])
 
     async function getHistory():Promise<void> {
-        // console.log("hello")
         try {
-            const response = await axios.get(`https://huawei-practice-web-backend.vercel.app/api/quiz/${quizId}`)
+            const response = await axios.get(`https://huawei-practice-web-backend.vercel.app/api/quiz?quizId=${quizId}&userId=${user.userId}`)
 
             if (response.status === 200) {
-                console.log(response.data.quiz)
-                // setScore(response.data.quiz.score)
-                // setType(response.data.quiz.type)
+                setScore(response.data[0].score)
             }
         }
         catch (err: any) {
@@ -30,9 +30,21 @@ function QuizResultHooks() {
         }
     }
 
-    console.log(quizId)
+    async function getQuizInfo():Promise<any> {
+        try {
+            const response = await axios.get(`https://huawei-practice-web-backend.vercel.app/api/quiz/${quizId}`)
 
-    return { quizId }
+            if (response.status === 200) {
+                // console.log(response.data)
+                setQuestions(response.data.quiz.questions)
+            }
+        }
+        catch (err: any) {
+            console.log(err)
+        }
+    }
+
+    return { quizId, score, questions }
 }
 
 export default QuizResultHooks
--- src/screens/quiz_result.tsx
+++ src/screens/quiz_result.tsx
@@ -1,17 +1,29 @@
+import QuestionCard from "../components/question_card"
 import QuizResultHooks from "../hooks/quiz_result_hooks"
 
 function QuizResult() {
 
-    const { quizId } = QuizResultHooks()
+    const { score, questions } = QuizResultHooks()
 
     return(
         <div>
-            <div className={`flex flex-col`}>
-                {/* <i className={`text-4xl ri-list-check-2 mb-2`}/> */}
+            <div className={`flex flex-col space-y-3`}>
+                <i className={`text-4xl ri-list-check-2`}/>
                 <h1 className={`text-xl font-semibold`}>Question Result</h1>
-                <h2 className={`text-gray-400 font-semibold`}>{ quizId }</h2>
-                {/* <h3>Score: {score}</h3>
-                <h3>Type: {type}</h3> */}
+                <h3><span className={`font-semibold`}>Score:</span> {score}</h3>
+                <h3 className={`font-semibold`}>Questions and the correct answer</h3>
+                <div>
+                {
+                    ...questions.map((question, index) => {
+                        return (
+                            <div className={`space-y-1`}>
+                                <h1 className={`text-md font-semibold`}>Question {index + 1}</h1>
+                                <QuestionCard question={question.question} answer={question.answer} options={question.options} type={question.type} id={question._id} /> 
+                            </div>
+                        )
+                    })
+                }
+                </div>
             </div>
         </div>
     )
